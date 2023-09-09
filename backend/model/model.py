0e921b72d013554cd5ba65ee40b0fae30bd6f274
Ramil Mavliutov <stirk2281337@gmail.com>
2023-09-09 15:26:33 +0500

    Add speech recognition

--- backend/model/model.py
+++ backend/model/model.py
@@ -1,4 +1,24 @@
+import subprocess
+
+import whisper
+
+
+class Converter:
+    def __init__(self):
+        pass
+
+
 class Model:
     def __init__(self):
         pass
 
+    @staticmethod
+    def automated_speech_recognition(path: str) -> str:
+        model = whisper.load_model('base')
+        result = model.transcribe(f'model/mp3/{path}')
+
+        return result['text']
+
+
+assistant_model = Model()
+converter = Converter()
--- backend/router.py
+++ backend/router.py
@@ -1,5 +1,8 @@
+import os
+import uuid
+
 from fastapi import APIRouter, UploadFile, File
-from model.model import Model
+from model.model import assistant_model, converter
 
 router = APIRouter(
     prefix='/assistant',
@@ -7,9 +10,14 @@ router = APIRouter(
 )
 
 
-# @router.get('/voice_problem')
-# async def get_voice_answer(file: UploadFile = File(...)):
-#     pass
+@router.post('/voice_problem')
+async def get_voice_answer(file: UploadFile = File(...)):
+    os.makedirs('model/mp3', exist_ok=True)
+    filename = str(uuid.uuid4()) + '.mp3'
+    file_path = os.path.join('model/mp3', filename)
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+    return assistant_model.automated_speech_recognition(filename)
 
 
 @router.get('/problem')
