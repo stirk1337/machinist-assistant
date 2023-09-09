5cc22b12e681a3ed7661eec68043982abea4a7f0
Serkik <63666583+Serckik@users.noreply.github.com>
2023-09-09 17:43:21 +0500

    новое решение

--- frontend-server/src/dictaphone.js
+++ frontend-server/src/dictaphone.js
@@ -1,41 +1,82 @@
-import React, { useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import axios from 'axios';
+import { Component } from "react";
+import { AudioRecorder } from 'react-audio-voice-recorder';
+import axios from "axios";
+import './App.css';
 
-const Dictaphone = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
-
-  function startListening(){
-    SpeechRecognition.startListening({ continuous: true, language: 'ru' })
-  }
-
-  async function stopListening(){
-    await SpeechRecognition.stopListening()
-    const data = {
-        text: transcript
+import { ReactMic } from 'react-mic';
+ 
+export default class Dictaphone extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      record: false,
+      text: ''
     }
+    this.stopRecording = this.stopRecording.bind(this)
+  }
+ 
+  startRecording = () => {
+    document.querySelector('.record-voice').classList.add('active')
+    console.log(this.state.record)
+    this.setState({ record: true });
+    if(this.state.record){
+      this.stopRecording()
+    }
+    this.handleText = this.handleText.bind(this)
+    this.onStop = this.onStop.bind(this)
+  }
+ 
+  stopRecording = () => {
+    document.querySelector('.record-voice').classList.remove('active')
+    this.setState({ record: false });
+    console.log(this.state.record)
+  }
+ 
+  onData(recordedBlob) {
+    console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  handleText(text){
+    this.setState({ text: text });
+  }
+ 
+  onStop = async (recordedBlob) => {
+    console.log('recordedBlob is: ', recordedBlob);
+    const audioBlob = await fetch(recordedBlob.blobURL).then((r) => r.blob());
+    const audiofile = new File([audioBlob], "audiofile.mpeg", {
+      type: "audio/mpeg",
+    });
+    var bodyFormData = new FormData();
+    bodyFormData.append('file', audiofile); 
+    console.log(audiofile)
     axios({
-        method: "post",
-        url: "http://100.73.214.19:8000/get_transcribe",
-        data: data
-      })
-    resetTranscript()
+      method: "post",
+      url: "http://localhost:8000/assistant/voice_problem",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+    .then(resolve => {
+      this.setState((state) => ({text: resolve.data}))
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
-
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
+ 
+  render() {
+      return (
+        <div className="voiceContainer">
+          <p>{this.state.text}</p>
+          <ReactMic
+            record={this.state.record}
+            className="sound-wave"
+            onStop={this.onStop}
+            onData={this.onData}
+            strokeColor="#000000"
+            backgroundColor="#FF4081" />
+          
+          <img className="record-voice" src="voice-logo.svg" alt="запись" onClick={this.startRecording}></img>
+        </div>
+      );
   }
-
-  return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onMouseDown={startListening} onMouseUp={stopListening}>{listening ? 'Говорите!' : 'Говорить'}</button>
-    </div>
-  );
-};
-export default Dictaphone;
+}
